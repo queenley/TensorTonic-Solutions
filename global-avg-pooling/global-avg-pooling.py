b663bcb8b72a4aa6e0d663fe9c44f3cfa0e49628
queenley <69098210+queenley@users.noreply.github.com>
2026-04-22 12:39:58 +0700

    Update solution for global-avg-pooling

--- global-avg-pooling/global-avg-pooling.py
+++ global-avg-pooling/global-avg-pooling.py
@@ -8,7 +8,8 @@ def global_avg_pool(x):
     # Write code here    
     x = np.array(x)
     if x.ndim not in (3, 4):
-        raise ValueError("ValueError")
+        raise ValueError(f"Input should be 3D (C,H,W) or 4D (N,C,H,W), "
+            f"received {x.ndim}D")
         
     gap = x.mean(axis=(-2, -1))
     return gap
